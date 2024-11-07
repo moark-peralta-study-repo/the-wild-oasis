caaaa9b147773187b568702c074b08765a2230f6
moark-peralta-study-repo <markperaltaasd@gmail.com>
2024-11-07 20:32:25 +0800

    added Button/Heading styled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles.js";
 import Button from "./ui/Button.jsx";
 import Input from "./ui/Input.jsx";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import Heading from "./ui/Heading.jsx";
+import Row from "./ui/Row.jsx";
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  //background-color: orangered;
   padding: 20px;
 `;
 
@@ -18,14 +15,37 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
-        <Button>Check in</Button>
-        <Button>Check in</Button>
+        >
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
 
-        <Input type="number" placeholder="No. of guests"></Input>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+
+              <Button variation="primary" size={"medium"}>
+                Check in
+              </Button>
+              <Button variatio="secondary" size="small">
+                Check in
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="No. of guests"></Input>
+              <Input type="number" placeholder="No. of guests"></Input>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
 }
+
+Row.defaultProps = {
+  type: "vertical",
+};
 
 export default App;
