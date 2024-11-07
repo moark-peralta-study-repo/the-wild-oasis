5d020daac49f32f3392f9f35060f365bfd52dcb0
moark-peralta-study-repo <markperaltaasd@gmail.com>
2024-11-07 18:47:37 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  margin: 20px;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <StyledApp>
+      <H1>The Wild Oasis</H1>
+      <Button>Check in</Button>
+      <Button>Check in</Button>
+
+      <Input type="number" placeholder="No. of guests"></Input>
+    </StyledApp>
+  );
+}
+
+export default App;
