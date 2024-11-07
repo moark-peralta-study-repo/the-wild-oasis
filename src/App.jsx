6231cc6ee0d18d252053063b2adaaabe9edf4479
moark-peralta-study-repo <markperaltaasd@gmail.com>
2024-11-07 19:31:33 +0800

    Button/Input styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
@@ -30,13 +15,16 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button>Check in</Button>
-      <Button>Check in</Button>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check in</Button>
+        <Button>Check in</Button>
 
-      <Input type="number" placeholder="No. of guests"></Input>
-    </StyledApp>
+        <Input type="number" placeholder="No. of guests"></Input>
+      </StyledApp>
+    </>
   );
 }
 
