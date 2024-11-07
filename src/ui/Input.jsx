a2165bda40e8cc0757862c25cf4a9a7b16d08661
moark-peralta-study-repo <markperaltaasd@gmail.com>
2024-11-07 19:45:32 +0800

    fixed typo in Input component

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -4,7 +4,7 @@ const Input = styled.input`
   border: 1px solid var(--color-grey-300);
   background-color: var(--color-grey-0);
   border-radius: var(--border-radius-sm);
-    box-sahdow: var(--shadow-sm)
+  box-shadow: var(--shadow-sm);
   padding: 0.8rem 1.2rem;
 `;
 
