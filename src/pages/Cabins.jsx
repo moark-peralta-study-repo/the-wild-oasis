21efa65e4964762ccd6527714cae3a33635881da
moark-peralta-study-repo <markperaltaasd@gmail.com>
2024-11-08 17:19:07 +0800

    delete cabin functionality

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,20 +1,19 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins.js";
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable.jsx";
 
 function Cabins() {
-  useEffect(() => {
-    return () => {
-      getCabins().then((data) => console.log(data));
-    };
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter/Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
