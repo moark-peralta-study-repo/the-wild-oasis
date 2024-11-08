6d5b915b14fc7019e1f6239c0e4d52839ca336f7
moark-peralta-study-repo <markperaltaasd@gmail.com>
2024-11-08 17:28:23 +0800

    added react toast

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Users from "./pages/Users.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -42,6 +43,29 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        toaster={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+        position="top-center"
+        gutter={12}
+        containerStyle={{
+          margin: "8px",
+        }}
+      />
     </QueryClientProvider>
   );
 }
