cae313eb9fbd058c5afe44731e88281a6ce68387
moark-peralta <peraltaramram@gmail.com>
2024-11-24 15:27:33 +0800

    initial auth logic

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,22 @@
+import { login as loginApi } from "../../services/apiAuth.js";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      //redirect to dashboard
+      navigate("/dashboard");
+    },
+    onError: (err) => {
+      console.log("ERROR", err);
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { login, isLoading };
+}
