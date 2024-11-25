3dbb07eb4159d7702bb020807a0bd9fedbb50d9d
moark-peralta <peraltaramram@gmail.com>
2024-11-26 07:05:02 +0800

    store user data to cache

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,16 @@
 import { login as loginApi } from "../../services/apiAuth.js";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      //redirect to dashboard
+      queryClient.setQueryData("user", user);
       navigate("/dashboard");
     },
     onError: (err) => {
