41c2c6481f65c80feca444cc7c6f7e9aa7964c08
moark-peralta <peraltaramram@gmail.com>
2024-11-26 17:30:59 +0800

    filter stays/bookings by days

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
+import { useRecentBookings } from "../features/dashboard/useRecentBookings.js";
+import { useRecentStays } from "../features/dashboard/useRecentStays.js";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import DashboardLayout from "../features/dashboard/DashboardLayout.jsx";
+import Spinner from "../ui/Spinner.jsx";
 import DashboardFilter from "../features/dashboard/DashboardFilter.jsx";
+import DashboardLayout from "../features/dashboard/DashboardLayout.jsx";
 
 function Dashboard() {
+  const { bookings, isLoadingBookings } = useRecentBookings();
+  const { stay, confirmedStays, isLoadingStays } = useRecentStays();
+
+  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+
+  console.log(bookings);
+
   return (
     <>
       <Row type="horizontal">
