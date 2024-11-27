8a19537cd9686f8f16a634910b26378bc744f2ed
moark-peralta <peraltaramram@gmail.com>
2024-11-27 18:03:06 +0800

    removed logging

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -81,8 +81,6 @@ function SalesChart({ bookings, numDays }) {
     };
   });
 
-  console.log(data);
-
   const colors = isDarkMode
     ? {
         totalSales: { stroke: "#4f46e5", fill: "#4f46e5" },
