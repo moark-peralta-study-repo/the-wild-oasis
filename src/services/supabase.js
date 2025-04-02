c368ac604bb416ef4c0c358f0170acf907c954eb
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-04-02 11:20:58 +0800

    supabols

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://ztwngiyufazuafmvilcz.supabase.co";
+export const supabaseUrl = "https://bbwwldbpdwokqhnjyltg.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0d25naXl1ZmF6dWFmbXZpbGN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwNDQ0OTMsImV4cCI6MjA0NjYyMDQ5M30.f_C1aI8VRPdSgxBaOHY0NcDpbSgj-9-h6tPVZCOa_1g";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJid3dsZGJwZHdva3Fobmp5bHRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4MTc2NTYsImV4cCI6MjA1ODM5MzY1Nn0.zauJL2BMhBE-Jym3FnhOWBcF5ifXNBjFbe1WJ5PhWj4";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
